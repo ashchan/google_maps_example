33c853661c6ad2a5d6a572980fb3c23293999fed
ashchan <ashchan@gmail.com>
2008-06-26 08:40:04 +0800

    chapter two

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,12 +1,28 @@
-var centerLatitude = 30.215168;
-var centerLongitude = 120.277977;
-var startZoom = 10;
+var map;
+var centerLatitude = 40.6897;
+var centerLongitude = -95.0446;
+var startZoom = 3;
+
+function addMarker(latitude, longitude, description) {
+	var marker = new GMarker(new GLatLng(latitude, longitude));
+	GEvent.addListener(marker, 'click', 
+		function() {
+			marker.openInfoWindowHtml(description);
+	});
+	
+	map.addOverlay(marker);
+}
 
 
 function init() {
   if (GBrowserIsCompatible()) {
-    var map = new GMap2(document.getElementById("map"));
+    map = new GMap2(document.getElementById("map"));
+	map.addControl(new GSmallMapControl());
     map.setCenter(new GLatLng(centerLatitude, centerLongitude), startZoom);
+
+	for (i = 0; i < markers.length; ++i) {
+		addMarker(markers[i].latitude, markers[i].longitude, markers[i].name);
+	}
   }
 }
 
